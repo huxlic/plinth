742350208f03cfe967109bd0c11cf3648919562f
huxlic <oladimejihassan03@gmail.com>
2026-07-14 19:39:45 +0100

    feat: add JetBrains Mono font to dependencies and import styles in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
+import "@fontsource/jetbrains-mono/100.css";
+import "@fontsource/jetbrains-mono/200.css";
+import "@fontsource/jetbrains-mono/300.css";
+import "@fontsource/jetbrains-mono/400.css";
+import "@fontsource/jetbrains-mono/500.css";
+import "@fontsource/jetbrains-mono/600.css";
+import "@fontsource/jetbrains-mono/700.css";
+import "@fontsource/jetbrains-mono/800.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
